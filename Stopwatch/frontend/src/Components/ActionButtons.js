85e5a04ba9b824c53c6b27e2173e1a03928010c9
Semir Pecar <91013287+freemayne@users.noreply.github.com>
2022-10-13 12:44:20 +0200

    mui actionbuttons

--- Stopwatch/frontend/src/Components/ActionButtons.js
+++ Stopwatch/frontend/src/Components/ActionButtons.js
@@ -2,24 +2,24 @@ import Button from "@mui/material/Button";
 import { Box } from "@mui/material";
 import React from "react";
 
-const ActionButtons = () => {
+const ActionButtons = (props) => {
   return (
-    <box className="buttons">
-      <button variant="contained" id="startTimer" onClick={props.handleStart}>
+    <Box className="buttons">
+      <Button variant="contained" id="startTimer" onClick={props.handleStart}>
         START
-      </button>
-      <button variant="contained" id="stopTimer" onClick={props.handleReset}>
+      </Button>
+      <Button variant="contained" id="stopTimer" onClick={props.handleReset}>
         STOP
-      </button>
-      <button
+      </Button>
+      <Button
         variant="contained"
         id="ResetTimer"
         onClick={props.handlePauseResume}
       >
         {props.isPaused ? "RESUME" : "STOP"}
         START
-      </button>
-    </box>
+      </Button>
+    </Box>
   );
 };
 
